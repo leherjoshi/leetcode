3182da127fe9bdcd37f340f404718d89aadb402c
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-07-05 07:46:48 +0530

    Time: 8 ms (78.87%), Space: 30.4 MB (83.82%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -3,34 +3,35 @@ public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        priority_queue<pair<int, int>> pq;
-        pq.push({0, 0});
+        vector<vector<int>> dist(n, vector<int>(m, -1));
+        priority_queue<pair<int,pair<int, int>>>pq;
+        pq.push({health-grid[0][0],{0, 0}});
         dist[0][0] = grid[0][0];
 
         while (!pq.empty()) {
-            auto [x, y] = pq.top();
+            auto [h,cell] = pq.top();
+            auto [x,y]=cell;
             pq.pop();
 
             int dx[] = {1, -1, 0, 0};
             int dy[] = {0, 0, -1, 1};
 
            
-
+            if(h<dist[x][y])continue;
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
                 if (nx < 0 || ny < 0 || nx >= n || ny >= m)
                     continue;
-                 int w = grid[nx][ny];
+                 int nh=h-grid[nx][ny];
 
-                if (dist[nx][ny] > dist[x][y] + w) {
-                    dist[nx][ny] = w + dist[x][y];
-                    pq.push({nx, ny});
+                if (nh > dist[nx][ny]) {
+                    dist[nx][ny] = nh;
+                    pq.push({nh,{nx, ny}});
                 }
             }
         }
-        return dist[n - 1][m - 1]<health;
+        return dist[n - 1][m - 1]>0;
     }
 };
