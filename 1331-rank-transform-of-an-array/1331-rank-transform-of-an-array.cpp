610dc711af1e04c33e75ecdec586128a0f57c994
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-07-12 23:06:39 +0530

    Time: 49 ms (30.4%), Space: 43.8 MB (64.79%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -3,24 +3,22 @@ public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         int n=arr.size();
         if(n==0)return {};
-        vector<int>mp(n);
+        unordered_map<int,int>mp;
+        int cnt=1;
         for(int i=0;i<n;i++){
-            mp[i]=i;
+            mp[arr[i]]=-1;
         }
-      sort(mp.begin(), mp.end(), [&arr](int a, int b) { 
-    return arr[a] < arr[b]; 
-});
-
+        vector<int>temp=arr;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n;i++){
+            if(mp[arr[i]]==-1){
+                mp[arr[i]]=cnt++;
+            }
+        }
         vector<int>ans(n);
-        ans[mp[0]]=1;
-        int rank=1;
-        for(int i=1;i<n;i++){
-            if(arr[mp[i]] != arr[mp[i-1]])
-                rank++;
-
-             ans[mp[i]] = rank;
+        for(int i=0;i<n;i++){
+            ans[i]=mp[temp[i]];
         }
-
         return ans;
     }
 };
