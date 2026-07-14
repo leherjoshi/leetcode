0c7e44446fbe427272016220056667eb011d695c
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-07-14 11:01:20 +0530

    Time: 15 ms (28.4%), Space: 16.6 MB (30.34%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool yes(string s,vector<string>&word){
+        for(int i=0;i<word.size();i++){
+            if(word[i]==s)return true;
+        }
+        return false;
+    }
+    bool solve(int i,string& s,vector<string>&word,vector<int>&dp){
+        if(i==s.size())return true;
+        if(dp[i]!=-1)return dp[i]==1;
+        for(int j=i;j<s.size();j++){
+            
+            if(yes(s.substr(i,j-i+1),word)){
+                if(solve(j+1,s,word,dp)){
+                return dp[j+1]=true;
+            }
+             }
+        }
+        return dp[i]=false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int>dp(s.size()+1,-1);
+        return solve(0,s,wordDict,dp);
+    }
+};
