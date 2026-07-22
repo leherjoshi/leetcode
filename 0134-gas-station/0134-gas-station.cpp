1fdfa134f78e8c425f21865965b4c0d704ca8d80
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-07-22 23:10:26 +0530

    Time: 3 ms (35.49%), Space: 112.6 MB (26.33%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,48 +1,16 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        vector<int> arr = {5,8,2,8};
-        vector<int> arr2 = {3,0,1,0,3};
-        vector<int> arr3 = {5,5,1,3,4};
-         vector<int> arr4 = {2,0,3};
-          vector<int> arr5 = {6,1,4,3,5};
-          vector<int> arr6 = {2,0,1,2,3,4,0};
-          vector<int> arr7 = {67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66};
-            if(gas[0]==3897)return 6690;
-          vector<int> arr9 = {3,3,3,3,3,3,3,3};
-
-            if (gas == arr) {
-                return 3;
-            }
-            if (gas == arr2) {
-                return 4;
-            }
-            if (gas == arr3) {
-                return 3;
-            }
-            if (gas == arr4) {
-                return 2;
-            }
-            if (gas == arr5) {
-                return 2;
-            }
-            if (gas == arr6) {
-                return 0;
-            }
-            if (gas == arr7) {
-                return 74;
-            }
-            if (gas == arr9) {
-                return 5;
-            }
+    
         int diff=0;
-        int mxdiff=INT_MIN;
-        int idx=-1;
+        int mxdiff=0;
+        int idx=0;
         for(int i=0;i<gas.size();i++){
                 diff+=gas[i]-cost[i];
-                if(mxdiff<gas[i]-cost[i]){
-                    mxdiff=gas[i]-cost[i];
-                    idx=i;
+                mxdiff+=gas[i]-cost[i];
+                if(mxdiff<0){
+                    mxdiff=0;
+                    idx=i+1;
                 }
                
         }
