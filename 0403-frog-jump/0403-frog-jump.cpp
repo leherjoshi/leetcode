33e0d8675976af216ece939fb85c613466fc496e
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-07-31 09:16:41 +0530

    Time: 337 ms (9.33%), Space: 589.9 MB (5%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,50 +1,35 @@
 class Solution {
 public:
-    int n;
-
-    bool solve(vector<int>& stones, int i, int last, vector<vector<int>>& dp) {
-
-        // Reached last stone
-        if (i == n - 1)
+    bool solve(vector<int>stones,int i,int last,vector<vector<int>>&dp){
+        if(i==stones.size()-1){
             return true;
-
-        // Memoization
-        if (dp[i][last] != -1)
+        }
+         if (dp[i][last] != -1)
             return dp[i][last];
 
-        // Try jumps: last-1, last, last+1
-        for (int d = -1; d <= 1; d++) {
-
-            int jump = last + d;
-
-            // Jump length must be positive
+        for(int d=-1;d<2;d++){
+            int jump=last+d;
             if (jump <= 0)
                 continue;
-
-            int nextPos = stones[i] + jump;
-
-            // Linear search for the next stone
-            for (int j = i + 1; j < n; j++) {
-
-                if (stones[j] == nextPos) {
-                    if (solve(stones, j, jump, dp))
-                        return dp[i][last] = true;
+            int nextpos=stones[i]+jump;
+            for(int j=i+1;j<stones.size();j++){
+                if(stones[j]==nextpos){
+                    if(solve(stones,j,jump,dp)){
+                        return dp[i][last]=true;
+                    }
                 }
-
-                // Stones are sorted
-                if (stones[j] > nextPos)
+                if(stones[j]>nextpos){
                     break;
+                }
             }
         }
-
-        return dp[i][last] = false;
+        return dp[i][last]=false;
     }
+   bool canCross(vector<int>& stones) {
 
-    bool canCross(vector<int>& stones) {
+        int n = stones.size();
 
-        n = stones.size();
-
-        // First jump must be 1
+        
         if (stones[1] != 1)
             return false;
 
