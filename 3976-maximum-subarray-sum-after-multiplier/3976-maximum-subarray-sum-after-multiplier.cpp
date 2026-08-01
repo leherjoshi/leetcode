aac8c6d0362e96baebd947b4751f90d203a5efed
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-01 16:44:30 +0530

    Time: 44 ms (61.79%), Space: 146.9 MB (62.81%) - LeetHub

--- 3976-maximum-subarray-sum-after-multiplier/3976-maximum-subarray-sum-after-multiplier.cpp
+++ 3976-maximum-subarray-sum-after-multiplier/3976-maximum-subarray-sum-after-multiplier.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    long long func(vector<int>& nums, vector<long long>&temp){
-        int n=nums.size();
-        long long mn = -4e18;
-        long long s0=mn, s1=mn, s2=mn, res=mn;
-
-        for(int i=0; i<n; i++){
-            long long x1=nums[i];
-            long long x2=temp[i];
-            long long n0 = max(s0+x1, x1);
-            long long n1 = max({x2,s0+x2,s1+x2});
-            long long n2 = max({s1 + x1, s2 + x1});
-
-            s0 =n0;
-            s1=n1;
-            s2=n2;
-            res=max({res,s1,s2});
-        }
-        return res;
-    }
     long long maxSubarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<long long>multii(n); vector<long long>div(n);
-        for(int i=0; i<n; i++){
-            multii[i]=1LL * nums[i] * k;
-            if(nums[i]>=0)div[i]=nums[i]/k;
-            else  div[i] = -((-nums[i]) / k);
+        int n = nums.size();
+        long long inf = numeric_limits<long long>::min() / 4;
+        
+        // f[0]=no op, f[1]=in multiply, f[2]=in divide, f[3]=after op
+        long long f[4] = {0, inf, inf, inf};
+        long long ans = inf;
+        
+        for (int x : nums) {
+            long long nf[4];
+            long long mul = 1LL * x * k;
+            long long div = x / k;  // C++ truncates toward 0: floor for +, ceil for -
+            
+            nf[0] = max(f[0], 0LL) + x;           // no operation yet
+            nf[1] = max({f[0], f[1], 0LL}) + mul; // currently multiplying
+            nf[2] = max({f[0], f[2], 0LL}) + div; // currently dividing
+            nf[3] = max({f[1], f[2], f[3]}) + x;  // operation ended, back to normal
+            
+            for (int i = 0; i < 4; ++i) f[i] = nf[i];
+            ans = max({ans, nf[0], nf[1], nf[2], nf[3]});
         }
-        return max(func(nums, multii), func(nums, div));
+        return ans;
     }
 };
