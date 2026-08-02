3e6c1594028088314482ded10d3c12b41658620e
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-02 17:57:47 +0530

    Time: 0 ms (100%), Space: 10.7 MB (39.52%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        return true;
+    bool solve(int i, int j, int alice, int bob, int turn,
+               vector<int>& piles) {
+
+        if (i > j)
+            return alice > bob;
+
+        if (turn == 0) {
+
+            return solve(i + 1, j, alice + piles[i], bob, 1, piles) ||
+                   solve(i, j - 1, alice + piles[j], bob, 1, piles);
+
+        } else {
+
+            return solve(i + 1, j, alice, bob + piles[i], 0, piles) &&
+                   solve(i, j - 1, alice, bob + piles[j], 0, piles);
+        }
+    }
+
+    bool stoneGame(vector<int>& A) {
+        int n=A.size();
+       vector<int> dp(n);
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = A[i];
+            for (int j = i + 1; j < n; j++)
+                dp[j] = max(A[i] - dp[j], A[j] - dp[j - 1]);
+        }
+
+        return dp[n - 1] > 0;
     }
 };
