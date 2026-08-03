6754c300750c5573486ac50287272fdc23cc2c4a
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-03 16:49:07 +0530

    Time: 617 ms (39.98%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,12 @@
+SELECT ROUND(
+    COUNT(*) / (SELECT COUNT(DISTINCT player_id) FROM Activity),
+    2
+) AS fraction
+FROM (
+    SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+) f
+JOIN Activity a
+ON f.player_id = a.player_id
+AND a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY);
