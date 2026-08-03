05c4bb7ff802d5eb41091ec3d00593f138e805a1
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-03 17:28:36 +0530

    Time: 0 ms (100%), Space: 8.9 MB (68.33%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.size();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
+            }else {
+                if(st.empty()) return false;
+                char c=st.top();st.pop();
+                if(s[i]==')'&&c!='('){
+                    return false;
+                }
+                 if(s[i]=='}'&&c!='{'){
+                    return false;
+                }
+                 if(s[i]==']'&&c!='['){
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
