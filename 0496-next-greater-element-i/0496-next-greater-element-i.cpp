494708adf8736cb874bafecb73171be7a89328fa
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-08 13:30:31 +0530

    Time: 4 ms (22.77%), Space: 12.8 MB (63.44%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        // vector<int> ans;
+        stack<int>st;
+        unordered_map<int,int>mp;
 
-        // for (int i = 0; i < nums1.size(); i++) {
-        //     int idx = -1;
+        for(int i=0;i<nums2.size();i++){
 
-        //     // Find the element in nums2
-        //     for (int j = 0; j < nums2.size(); j++) {
-        //         if (nums2[j] == nums1[i]) {
-        //             idx = j;
-        //             break;
-        //         }
-        //     }
-
-        //     int greater = -1;
-
-        //     // Search to the right
-        //     for (int j = idx + 1; j < nums2.size(); j++) {
-        //         if (nums2[j] > nums1[i]) {
-        //             greater = nums2[j];
-        //             break;
-        //         }
-        //     }
-
-        //     ans.push_back(greater);
-        // }
-
-            stack<int>st;
-            unordered_map<int,int>mp;
-            for(int i=0;i<nums2.size();i++){
-                while(!st.empty()&&st.top()<=nums2[i]){
-                    mp[st.top()]=nums2[i];
-                    st.pop();
-                }
-                st.push(nums2[i]);
+            while(!st.empty()&&st.top()<=nums2[i]){
+                mp[st.top()]=nums2[i];
+                st.pop();
             }
-            while(!st.empty()){
+            st.push(nums2[i]);
+        }
+        while(!st.empty()){
                     mp[st.top()]=-1;
                     st.pop();
                 }
@@ -46,4 +22,5 @@ public:
             }
         return ans;
     }
+
 };
