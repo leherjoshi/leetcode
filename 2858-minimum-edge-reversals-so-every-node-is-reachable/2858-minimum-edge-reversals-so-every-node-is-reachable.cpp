9dd94e116a8ef5866d822af3ad9970dfb74d2e4f
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-10 08:43:08 +0530

    Time: 241 ms (35.47%), Space: 230.4 MB (36.04%) - LeetHub

--- 2858-minimum-edge-reversals-so-every-node-is-reachable/2858-minimum-edge-reversals-so-every-node-is-reachable.cpp
+++ 2858-minimum-edge-reversals-so-every-node-is-reachable/2858-minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
-    vector<vector<pair<int,int>>> adj;
-    vector<int> ans;
 
-    void dfs(int src, int p) {
-        for (auto &[child, wt] : adj[src]) {
-            if (child != p) {
-                ans[0] += wt;
-                dfs(child, src);
+    vector<vector<pair<int,int>>>adj;
+    vector<int>ans;
+
+    void dfs(int node,int parent){
+        for (auto &[child, wt] : adj[node]) {
+            if (child != parent) {
+            ans[0]+=wt;
+            dfs(child,node);
             }
         }
     }
 
-    void dfs2(int src, int p) {
-        for (auto &[child, wt] : adj[src]) {
-            if (child != p) {
-
-                // wt = 0 : src -> child (already correct)
-                // wt = 1 : child -> src (needs reversal)
-
-                ans[child] = ans[src] + (wt == 0 ? 1 : -1);
-
-                dfs2(child, src);
+    void reroot(int node,int parent){
+        for(auto &[child,wt]:adj[node]){
+            if(child!=parent){
+                ans[child]=ans[node]+(wt==0?1:-1);
+                reroot(child,node);
             }
         }
     }
-
     vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
-        adj.assign(n, {});
-        ans.assign(n, 0);
-
-        for (auto &e : edges) {
-            adj[e[0]].push_back({e[1], 0});
-            adj[e[1]].push_back({e[0], 1});
+        adj.assign(n,{});
+        ans.assign(n,0);
+        for(auto e:edges){
+            adj[e[0]].push_back({e[1],0});
+            adj[e[1]].push_back({e[0],1});
         }
-
-        // Calculate answer for root 0
-        dfs(0, -1);
-
-        // Calculate answers for all other nodes
-        dfs2(0, -1);
-
+        dfs(0,-1);
+        reroot(0,-1);
         return ans;
+
     }
 };
