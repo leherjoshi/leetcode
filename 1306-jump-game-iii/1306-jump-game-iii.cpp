c9dbbfccd9cdc85b2eba4d6091da7435f9d10646
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-14 14:42:06 +0530

    Time: 0 ms (100%), Space: 31.4 MB (85.21%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool solve(int i,vector<int>&arr,vector<int>&vis){
+        if(i<0||i>=arr.size()||vis[i]==1)return false;
+
+        if(arr[i]==0)return true;
+        
+        vis[i]=1;
+        return solve(i+arr[i],arr,vis)||solve(i-arr[i],arr,vis);
+    }
+    bool canReach(vector<int>& arr, int start) {
+        vector<int>vis(arr.size(),0);
+        return solve(start,arr,vis);
+    }
+};
