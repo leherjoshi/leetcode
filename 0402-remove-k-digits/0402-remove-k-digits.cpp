151605aa1b4289eeaaa7eafc68eeb2dba4af99de
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-19 17:37:29 +0530

    Time: 0 ms (100%), Space: 11.7 MB (30.74%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        stack<char> st;
+        string s="";
+        stack<char>st;
 
-        for(int i = 0; i < num.size(); i++) {
-            while(k > 0 && !st.empty() && st.top() > num[i]) {
+        for(int i=0;i<num.size();i++){
+            while(k>0&&!st.empty()&&st.top()>num[i]){
                 st.pop();
                 k--;
             }
             st.push(num[i]);
         }
-
-        
-        while(k > 0 && !st.empty()) {
+        while(k>0&&!st.empty()){
             st.pop();
             k--;
         }
-
-        string s = "";
-        while(!st.empty()) {
-            s += st.top();
+          while(!st.empty()){
+            s+=st.top();
             st.pop();
         }
 
-        reverse(s.begin(), s.end());
-
-       
-        int i = 0;
-        while(i < s.size() && s[i] == '0') {
-            i++;
+        reverse(s.begin(),s.end());
+        int i;
+        for(i=0;i<s.size();i++){
+            if(s[i]!='0')break;
+            
         }
-
-        s = s.substr(i);
-
-        if(s == "") return "0";
+        s=s.substr(i);
+        if(s=="")return "0";
         return s;
     }
 };
