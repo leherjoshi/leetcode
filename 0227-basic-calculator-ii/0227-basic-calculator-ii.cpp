823f4c54cde25e106ec2733150e07ad70aa1917b
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-19 19:41:27 +0530

    Time: 4 ms (81.53%), Space: 15.3 MB (28.48%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
     int calculate(string s) {
-       stack<int>st;
-       int num=0;
-       char op='+';
+        stack<int> st;
+        int num = 0;
+        char op = '+';
 
-       for(int i=0;i<s.size();i++){
-        if(isdigit(s[i])){
-            num=num*10+(s[i]-'0');
-        }
-        if((!isdigit(s[i])&&s[i]!=' ')||i==s.size()-1){
-            if(op=='+'){
-                st.push(num);
-            }else if(op=='-'){
-                st.push(-num);
-            }else if(op=='*'){
-                int x=st.top();
-                st.pop();
-                st.push(x*num);
-            }else if(op=='/'){
-                int x=st.top();
-                st.pop();
-                st.push(x/num);
+        for(int i = 0; i < s.size(); i++) {
+
+            if(isdigit(s[i])) {
+                num = num * 10 + (s[i] - '0');
             }
-            op=s[i];
-            num=0;
+
+            if((!isdigit(s[i]) && s[i] != ' ') || i == s.size() - 1) {
+
+                if(op == '+') {
+                    st.push(num);
+                }
+                else if(op == '-') {
+                    st.push(-num);
+                }
+                else if(op == '*') {
+                    int x = st.top();
+                    st.pop();
+                    st.push(x * num);
+                }
+                else if(op == '/') {
+                    int x = st.top();
+                    st.pop();
+                    st.push(x / num);
+                }
+
+                op = s[i];
+                num = 0;
+            }
         }
-       }
-       int ans=0;
-       while(!st.empty()){
-        ans+=st.top();
-        st.pop();
-       }
-       return ans;
+
+        int res = 0;
+
+        while(!st.empty()) {
+            res += st.top();
+            st.pop();
+        }
+
+        return res;
     }
 };
