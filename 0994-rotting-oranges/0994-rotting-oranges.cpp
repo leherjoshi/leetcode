5831cca0ab0576541eda9024f27d9e7a0bc8fa27
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-20 10:31:43 +0530

    Time: 2 ms (30.59%), Space: 16.9 MB (51.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,53 +1,54 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-     
-      queue<pair<int, pair<int,int>>> q;
+        int fresh = 0;
 
+        queue<pair<int,pair<int,int>>> q;
 
-        int n=grid.size();
-        int m=grid[0].size();
-       vector<vector<int>>vis=grid;
-        int cnt=0;
-   
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
-                    cnt++;                    
-                }else if(grid[i][j]==2){
-                    q.push({0,{i,j}});
-                   
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 2) {
+                    q.push({0, {i,j}});
+                }
+                else if(grid[i][j] == 1) {
+                    fresh++;
                 }
             }
         }
-        int tm=0;
-        while(!q.empty()){
-            auto [t,cell]=q.front();
+
+        int time_max = 0;
+        int rotton = 0;
+
+        while(!q.empty()) {
+            auto [t, cell] = q.front();
             q.pop();
 
-            int row=cell.first;
-            int col=cell.second;
+            time_max = max(t, time_max);
 
-            tm=max(tm,t);
-            
+            int row = cell.first;
+            int col = cell.second;
 
-            int dx[]={-1,1,0,0};
-            int dy[]={0,0,-1,1};
+            int dx[] = {1,-1,0,0};
+            int dy[] = {0,0,-1,1};
 
-            for(int dir=0;dir<4;dir++){
-                int nx=row+dx[dir];
-                int ny=col+dy[dir];
+            for(int k = 0; k < 4; k++) {
+                int nx = row + dx[k];
+                int ny = col + dy[k];
 
-                if(nx>=0&&nx<n&&ny>=0&&ny<m){
-                    if(vis[nx][ny]==1){
-                        vis[nx][ny]=2;
-                        q.push({t+1,{nx,ny}});
-                        cnt--;
-                    }
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m 
+                   && grid[nx][ny] == 1) {
+
+                    rotton++;
+                    grid[nx][ny] = 2;
+
+                    q.push({t+1, {nx,ny}});
                 }
-            }                        
-        }        
-        return cnt>0?-1:tm;
+            }
+        }
 
+        return (rotton == fresh) ? time_max : -1;
     }
 };
