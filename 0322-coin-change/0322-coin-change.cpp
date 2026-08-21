f9ec21db67df04f30d3fd0dd54510899c78a3674
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-21 08:44:25 +0530

    Time: 57 ms (16.85%), Space: 49.7 MB (24.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-            int n=coins.size();
-        vector<int>dp(amount+1,100000);
-        dp[0]=0;
-
-        for (int i = 0; i < n; i++) {
-                        for (int j = coins[i]; j <= amount; j++) {
-
-                dp[j]=min(dp[j],dp[j-coins[i]]+1);
-            }
+    int solve(int i,vector<int>&coins,int amount,vector<vector<int>>&dp){
+        if(amount==0)return 0;
+        if(i==coins.size())return 1e9;
+        if(dp[i][amount]!=-1)return dp[i][amount];
+        int take=1e9;
+        int notake =solve(i+1,coins,amount,dp);
+        if(amount>=coins[i]){
+            take=1+solve(i,coins,amount-coins[i],dp);
         }
-        return (dp[amount]==100000)?-1:dp[amount];
+        return dp[i][amount]=min(take,notake);
+
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+        int ans = solve(0, coins, amount,dp);
+        
+        if(ans == 1e9) return -1;
+
+        return ans;
     }
 };
