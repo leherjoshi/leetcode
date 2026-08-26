60cf13e7f8e613dfeb957686fe8f4599619891ed
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-26 18:00:54 +0530

    Time: 0 ms (100%), Space: 8.6 MB (5.49%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        unsigned int num = 1;  // Start with bit position 0, not 4294967295
+       
         vector<int> store;
         
         for (int i = 0; i < 32; i++) {  // Use a loop instead of while
             int bit = (n >> i) & 1;  // Extract the i-th bit properly
             store.push_back(bit);    // Store 0 or 1
-            num = num << 1;
+          
         }
         
         int res = 0;
