416a839810477c40fba289d5eb31bb96fb60842f
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-26 19:08:38 +0530

    Time: 0 ms (100%), Space: 10.3 MB (75.24%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    int fun(int i,vector<int>&nums,vector<int>&dp){
-        // if(i==nums.size()-1)return nums[i];
-        if(i>=nums.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int m1=nums[i]+fun(i+2,nums,dp);
-        int m2=fun(i+1,nums,dp);
-        return dp[i]=max(m1,m2);
+    int fun(int n,vector<int>& nums){
+      
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
 
-    }
-
- 
+        int a = nums[0], b = max(nums[0], nums[1]);
+        for(int i = 2; i < n; i++) {
+            int curr = max(nums[i]+a, b);
+            a = b;
+            b = curr;
+        }
+        return b;
+}
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n<2)return nums[0];
-       vector<int>temp1,temp2;
-       for(int i=0;i<n;i++){
+       int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int>temp1,temp2;
+        for(int i = 0; i < n; i++) {
         if(i!=0)temp1.push_back(nums[i]);
         if(i!=n-1)temp2.push_back(nums[i]);
-
-       } 
-       
-        vector<int>dp1(n,-1);
-        
-        vector<int>dp2(n,-1);
-       return max({fun(0,temp1,dp1),fun(0,temp2,dp2)});
+        }
+        return max(fun(n-1,temp1),fun(n-1,temp2));
     }
 };
