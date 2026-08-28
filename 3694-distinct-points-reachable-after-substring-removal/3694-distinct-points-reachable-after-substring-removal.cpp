82520d5795a2b204c317376d3a675a699dd04418
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-28 20:00:23 +0530

    Time: 132 ms (47.43%), Space: 38.7 MB (67.09%) - LeetHub

--- 3694-distinct-points-reachable-after-substring-removal/3694-distinct-points-reachable-after-substring-removal.cpp
+++ 3694-distinct-points-reachable-after-substring-removal/3694-distinct-points-reachable-after-substring-removal.cpp
@@ -1,22 +1,46 @@
 class Solution {
 public:
     int distinctPoints(string s, int k) {
-      set<pair<int,int>>seen;
-      seen.insert({0,0});
-      int x=0;
-      int y=0;
-      for(int i=k;i<s.size();i++){
-        if(s[i]=='U')y++;
-        if(s[i]=='D')y--;
-        if(s[i]=='L')x++;
-        if(s[i]=='R')x--;
+        set<pair<int,int>> seen;
 
-        if(s[i-k]=='U')y--;
-        if(s[i-k]=='D')y++;
-        if(s[i-k]=='L')x--;
-        if(s[i-k]=='R')x++;
-        seen.insert({x,y});
-      }  
-      return seen.size();
+        int x = 0, y = 0;
+
+        // Total movement
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == 'U') y++;
+            if(s[i] == 'D') y--;
+            if(s[i] == 'L') x--;
+            if(s[i] == 'R') x++;
+        }
+
+        // Initially remove first k characters
+        for(int i = 0; i < k; i++) {
+            if(s[i] == 'U') y--;
+            if(s[i] == 'D') y++;
+            if(s[i] == 'L') x++;
+            if(s[i] == 'R') x--;
+        }
+
+        seen.insert({x, y});
+
+        // Slide window
+        for(int i = k; i < s.size(); i++) {
+
+            // Add new character back
+            if(s[i] == 'U') y--;
+            if(s[i] == 'D') y++;
+            if(s[i] == 'L') x++;
+            if(s[i] == 'R') x--;
+
+            // Remove old character
+            if(s[i-k] == 'U') y++;
+            if(s[i-k] == 'D') y--;
+            if(s[i-k] == 'L') x--;
+            if(s[i-k] == 'R') x++;
+
+            seen.insert({x, y});
+        }
+
+        return seen.size();
     }
 };
