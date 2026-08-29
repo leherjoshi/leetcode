34cdb98f64759740dfa1709d884fdf7a361acebf
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-29 19:30:46 +0530

    Time: 0 ms (100%), Space: 8.2 MB (24.56%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        int n=s.size();
+        int countE=0;
+        int countD=0;
+        int countN=0;
+        int countAE=0;
+
+        for(int i=0;i<n;i++){
+
+            if(s[i]=='.') countD++;
+
+            if(s[i]=='e'||s[i]=='E') countE++;
+
+            if(isdigit(s[i])){
+                if(countE) countAE++;
+                else countN++;
+            }
+
+            if(s[i]=='+'||s[i]=='-'){
+                if(i!=0 && s[i-1]!='e' && s[i-1]!='E')
+                    return false;
+
+                if(i==n-1)
+                    return false;
+            }
+
+            if(isalpha(s[i])&&(s[i]!='e'&&s[i]!='E'))
+                return false;
+
+            if(countD>1 || countE>1)
+                return false;
+
+            if(s[i]=='.' && countE)
+                return false;
+
+            if(i==0 && (s[i]=='e'||s[i]=='E'))
+                return false;
+
+            if(i==n-1 && (s[i]=='e'||s[i]=='E'))
+                return false;
+
+            if((s[i]=='e'||s[i]=='E') && !countN)
+                return false;
+        }
+
+        if((countE && !countAE) || !countN)
+            return false;
+
+        return true;
+    }
+};
