8b86b40e9723b4c07a599e77508167bd080d8ddd
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-29 12:55:11 +0530

    Time: 22 ms (61.95%), Space: 24 MB (38.47%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,56 +1,49 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int> result;
-        if (s.empty() || words.empty()) return result;
+        vector<int>res;
+        int wordLen=words[0].size();
+        int numlen=words.size();
+        int windowlen=wordLen*numlen;
+        int n=s.size();
 
-        int wordLen = words[0].size();
-        int numWords = words.size();
-        int windowLen = wordLen * numWords;
-        int n = s.size();
-        if (n < windowLen) return result;
+        unordered_map<string,int>target;
+        for(string st:words)target[st]++;
 
-        unordered_map<string, int> target;
-        for (auto& w : words) target[w]++;
+       for(int offset=0;offset<wordLen;offset++){
+        int left=offset;
+        int count=0;
+        unordered_map<string,int>window;
+            
+            for(int right=offset;right+wordLen<=n;right+=wordLen){
+                string w=s.substr(right,wordLen);
 
-        for (int offset = 0; offset < wordLen; offset++) {
-            int left = offset;
-            int count = 0; // number of words currently matched in window
-            unordered_map<string, int> window;
-
-            for (int right = offset; right + wordLen <= n; right += wordLen) {
-                string w = s.substr(right, wordLen);
-
-                if (target.find(w) == target.end()) {
-                    // word not in target: reset window
+                if(target.find(w)==target.end()){
                     window.clear();
-                    count = 0;
-                    left = right + wordLen;
+                    count=0;
+                    left=right+wordLen;
                     continue;
                 }
-
                 window[w]++;
                 count++;
 
-                // if this word now exceeds needed count, shrink from left
-                while (window[w] > target[w]) {
-                    string leftWord = s.substr(left, wordLen);
-                    window[leftWord]--;
+                while(window[w]>target[w]){
+                    string leftword=s.substr(left,wordLen);
+                    window[leftword]--;
                     count--;
-                    left += wordLen;
+                    left+=wordLen;
                 }
 
-                if (count == numWords) {
-                    result.push_back(left);
-                    // slide left forward by one word to look for next match
-                    string leftWord = s.substr(left, wordLen);
-                    window[leftWord]--;
+                if(count==numlen){
+                    res.push_back(left);
+                    string leftword=s.substr(left,wordLen);
+                    window[leftword]--;
                     count--;
-                    left += wordLen;
+                    left+=wordLen;
                 }
+
             }
-        }
-
-        return result;
+       }
+       return res;
     }
 };
