8642ce794358609664d149794e2c6db523adcbaa
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-30 19:14:39 +0530

    Time: 7 ms (7.99%), Space: 14.7 MB (53.64%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+   // vector<int
+    vector<int> grayCode(int n) {
+        if(n==0)return {0};
+
+        vector<int>ans=grayCode(n-1);
+        for(int i=ans.size()-1;i>=0;i--){
+            ans.push_back(ans[i]+(1<<(n-1)));
+        }
+        return ans;
+    }
+};
