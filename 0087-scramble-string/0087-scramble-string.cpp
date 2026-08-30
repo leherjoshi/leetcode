424c8a08f59c1c35d5e5ffb1a162c2a8607f6fcc
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-30 14:51:59 +0530

    Time: 12 ms (73.95%), Space: 12.5 MB (91.98%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,86 +1,43 @@
 class Solution {
 public:
-//for storing already solved problems
-    unordered_map<string,bool> mp;
-    
-    
-    bool isScramble(string s1, string s2) {
-        //base cases
-        
-        int n = s1.size();
-        
-        //if both string are not equal in size
-        if(s2.size()!=n)
-            return false;
-        
-        //if both string are equal
-        if(s1==s2)
-         return true;   
-        
-            
-        
-        //if code is reached to this condition then following this are sure:
-        //1. size of both string is equal
-        //2.  string are not equal
-        //so size is equal (where size==1) and they are not equal then obviously false
-        //example 'a' and 'b' size is equal ,string are not equal
-        if(n==1)
-            return false;
-        
-        string key = s1+" "+s2;
-        
-		//check if this problem has already been solved
-        if(mp.find(key)!=mp.end())
-            return mp[key];
-        
-        //for every iteration it can two condition 
-        //1.we should proceed without swapping
-        //2.we should swap before looking next
-        for(int i=1;i<n;i++)
-        {
+    int dp[31][31][31];
 
-            //ex of without swap: gr|eat and rg|eat
-            bool withoutswap = (
-                //left part of first and second string
-                isScramble(s1.substr(0,i),s2.substr(0,i)) 
-                
-                &&
-                
-                //right part of first and second string;
-                isScramble(s1.substr(i),s2.substr(i))
-                );
-            
-            
-            
-            //if without swap give us right answer then we do not need 
-            //to call the recursion withswap
-            if(withoutswap)
-                return true;
-            
-            //ex of withswap: gr|eat  rge|at
-			//here we compare "gr" with "at" and "eat" with "rge"
-            bool withswap = (
-                //left part of first and right part of second 
-                isScramble(s1.substr(0,i),s2.substr(n-i)) 
-                
-                &&
-                
-                //right part of first and left part of second
-                isScramble(s1.substr(i),s2.substr(0,n-i)) 
-                );
-            
-            
-            
-            //if withswap give us right answer then we return true
-            //otherwise the for loop do it work
-            if(withswap)
-                return true;
-            //we are not returning false in else case 
-            //because we want to check further cases with the for loop
-        }
-        
-        
-        return mp[key] = false;
-        
+    bool solve(int i, int j, int len, string &s1, string &s2) {
+        if(len == 1)
+            return s1[i] == s2[j];
+
+        if(dp[i][j][len] != -1)
+            return dp[i][j][len];
+
+        if(s1.substr(i, len) == s2.substr(j, len))
+            return dp[i][j][len] = true;
+
+        return split(i, j, len, s1, s2, 1);
+    }
+
+    bool split(int i, int j, int len, string &s1, string &s2, int k) {
+        if(k == len)
+            return dp[i][j][len] = false;
+
+        // No swap
+        if(solve(i, j, k, s1, s2) &&
+           solve(i+k, j+k, len-k, s1, s2))
+            return dp[i][j][len] = true;
+
+        // Swap
+        if(solve(i, j+len-k, k, s1, s2) &&
+           solve(i+k, j, len-k, s1, s2))
+            return dp[i][j][len] = true;
+
+        return split(i, j, len, s1, s2, k+1);
+    }
+
+    bool isScramble(string s1, string s2) {
+        if(s1.size() != s2.size())
+            return false;
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(0, 0, s1.size(), s1, s2);
     }
 };
