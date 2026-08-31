4a52f139db4c952ab025cb63ec69877cabb559f5
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-08-31 10:21:34 +0530

    Time: 0 ms (100%), Space: 10.8 MB (43.39%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -10,37 +10,14 @@ public:
             return;
         }
 
-        if(i >= s.size())
-            return;
-
-        // send 1 digit
-        solve(i + 1, mid + s[i] + '.', ans, s, parts + 1);
-
-        // send 2 digits
-        if(s[i] != '0' && i + 1 < s.size()) {
-            string st = "";
-
-            for(int j = i; j < i + 2; j++) {
-                st += s[j];
-            }
-
-            if(stoi(st) <= 255) {
-                solve(i + 2, mid + st + '.', ans, s, parts + 1);
-            }
-        }
-
-        // send 3 digits
-        if(s[i] != '0' && i + 2 < s.size()) {
-            string st = "";
-
-            for(int j = i; j < i + 3; j++) {
-                st += s[j];
-            }
-
-            if(stoi(st) <= 255) {
-                solve(i + 3, mid + st + '.', ans, s, parts + 1);
-            }
-        }
+       string st="";
+       
+       for(int j =i;j<i+3&&j<s.size();j++){
+        st+=s[j];
+        if(st.size()>1&&st[0]=='0')break;
+        if(stoi(st)>255)break;
+        solve(j+1,mid+st+'.',ans,s,parts+1);
+       }
     }
 
     vector<string> restoreIpAddresses(string s) {
