bf6725b676c9dd1c126b027e64b9ac4bc002e0f7
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-04 18:40:09 +0530

    Time: 548 ms (27.79%), Space: 240.4 MB (55.18%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,69 +1,54 @@
 class Solution {
 public:
-    int cnt = 0;
-
-    void merge(vector<int>& nums, int left, int mid, int right) {
-        vector<int> temp;
-
-        int i = left;
-        int j = mid + 1;
-
-        while (i <= mid && j <= right) {
-            if (nums[i] <= nums[j]) {
+     int cnt=0;
+    void merge(vector<int>&nums,int left,int mid,int right){
+        vector<int>temp;
+        int i=left;
+        int j=mid+1;
+        
+        while(i<=mid && j<=right){
+            if(nums[i]<=nums[j]){
                 temp.push_back(nums[i]);
                 i++;
-            } else {
+            }else{
                 temp.push_back(nums[j]);
                 j++;
             }
         }
 
-        while (i <= mid) {
+        while(i<=mid){
             temp.push_back(nums[i]);
             i++;
         }
-
-        while (j <= right) {
+        while(j<=right){
             temp.push_back(nums[j]);
             j++;
         }
 
-        for (int k = 0; k < temp.size(); k++) {
-            nums[left + k] = temp[k];
+        for(int k=0;k<temp.size();k++){
+            nums[left+k]=temp[k];
         }
     }
+    void mergesort(vector<int>&nums,int i,int j){
+        if(i>=j)return ;
 
-    void mergesort(vector<int>& nums, int i, int j) {
-        if (i >= j) {
-            return;
-        }
+        int mid=(i+j)/2;
 
-        int mid = (i + j) / 2;
+        mergesort(nums,i,mid);
+        mergesort(nums,mid+1,j);
 
-        mergesort(nums, i, mid);
-        mergesort(nums, mid + 1, j);
-
-        // Count reverse pairs
-        int right = mid + 1;
-
-        for (int k = i; k <= mid; k++) {
-            while (right <= j &&
-                   (long long)nums[k] > 2LL * nums[right]) {
+        int right=mid+1;
+        for(int k=i;k<=mid;k++){
+            while(right<=j && (long long)nums[k]>2LL*nums[right]){
                 right++;
             }
-
-            cnt += right - (mid + 1);
+            cnt+=right-(mid+1);
         }
-
-        // Merge both sorted halves
-        merge(nums, i, mid, j);
+        merge(nums,i,mid,j);
     }
-
     int reversePairs(vector<int>& nums) {
-        cnt = 0;
-
-        mergesort(nums, 0, nums.size() - 1);
-
+        cnt=0;
+        mergesort(nums,0,nums.size()-1);
         return cnt;
     }
 };
