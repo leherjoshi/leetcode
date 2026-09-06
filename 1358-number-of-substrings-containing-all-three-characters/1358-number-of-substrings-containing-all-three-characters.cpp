58be2cdcd811c30b26e571736a3db9d965a68b32
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-06 11:28:18 +0530

    Time: 33 ms (18.58%), Space: 16.8 MB (8.1%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int atMost(string s,int k){
-        unordered_map<char,int>mp;
+    // int atMost(string s,int k){
+    //     unordered_map<char,int>mp;
 
-        int left=0;
-        int count=0;
-        int len=0;
-        for(int right=0;right<s.size();right++){
-            mp[s[right]]++;
-            while(mp.size()>k){
-                mp[left]--;
-                if(mp[s[left]]==0)mp.erase(s[left]);
-                left++;
+    //     int left=0;
+    //     int count=0;
+    //     int len=0;
+    //     for(int right=0;right<s.size();right++){
+    //         mp[s[right]]++;
+    //         while(mp.size()>k){
+    //             mp[left]--;
+    //             if(mp[s[left]]==0)mp.erase(s[left]);
+    //             left++;
 
-            }
-            len+=right-left+1;
-        }
-        return len;
-    }
+    //         }
+    //         len+=right-left+1;
+    //     }
+    //     return len;
+    // }
     int numberOfSubstrings(string s) {
         unordered_map<char,int>mp;
 
