ff7336f893fc553e356181af43eccefe3c197a4f
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-08 15:53:56 +0530

    Time: 41 ms (42.78%), Space: 31 MB (87.61%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-
-        if (n % groupSize != 0)
-            return false;
-
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < n; i++) {
+    bool isNStraightHand(vector<int>& hand, int groupsize) {
+         if(hand.size() % groupsize != 0) return false;
+        sort(hand.begin(),hand.end());
+        int cnt=0;
+        unordered_map<int,int>mp;
+        for(int i=0;i<hand.size();i++){
             mp[hand[i]]++;
         }
 
-        sort(hand.begin(), hand.end());
+        for(int i=0;i<hand.size();i++){
+            int x=hand[i];
+            if(mp[x]==0)continue;
+            mp[x]--;
+            int cnt=1;
+            while(cnt<groupsize){
 
-        for (int i = 0; i < n; i++) {
-            int x = hand[i];
-
-            if (mp[x] == 0)
-                continue;
-
-            for (int j = 0; j < groupSize; j++) {
-                if (mp[x + j] == 0)
-                    return false;
-
-                mp[x + j]--;
+                if(mp[x+1]==0)return false;
+                mp[x+1]--;
+                cnt++;
+                x++;
             }
+            
         }
-
         return true;
+       
     }
 };
