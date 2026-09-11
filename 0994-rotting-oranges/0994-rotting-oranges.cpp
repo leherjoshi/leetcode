0ae25a3427cd7ae4f4e99ad59a05a4c8b0d7c8dd
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-11 23:46:28 +0530

    Time: 0 ms (100%), Space: 16.6 MB (97.49%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int fresh = 0;
+        int n=grid.size();
+        int m=grid[0].size();
 
-        queue<pair<int,pair<int,int>>> q;
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 2) {
-                    q.push({0, {i,j}});
-                }
-                else if(grid[i][j] == 1) {
+        int fresh =0;
+        queue<pair<int,pair<int,int>>>q;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    q.push({0,{i,j}});
+                }else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
 
-        int time_max = 0;
-        int rotton = 0;
 
-        while(!q.empty()) {
-            auto [t, cell] = q.front();
+        int tm=0;
+
+        while(!q.empty()){
+            int t=q.front().first;
+            int i= q.front().second.first;
+            int j=q.front().second.second;
             q.pop();
+            tm=max(tm,t);
 
-            time_max = max(t, time_max);
+            int dx[]={-1,1,0,0};
+            int dy[]={0,0,-1,1};
 
-            int row = cell.first;
-            int col = cell.second;
-
-            int dx[] = {1,-1,0,0};
-            int dy[] = {0,0,-1,1};
-
-            for(int k = 0; k < 4; k++) {
-                int nx = row + dx[k];
-                int ny = col + dy[k];
-
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m 
-                   && grid[nx][ny] == 1) {
-
-                    rotton++;
-                    grid[nx][ny] = 2;
-
-                    q.push({t+1, {nx,ny}});
+            for(int d=0;d<4;d++){
+                int nx=i+dx[d];
+                int ny=j+dy[d];
+                if(nx>=0 &&nx<n &&ny>=0 &&ny<m && grid[nx][ny]==1){
+                    grid[nx][ny]=2;
+                    q.push({tm+1,{nx,ny}});
+                    fresh--;
                 }
             }
         }
+        return fresh==0?tm:-1;
 
-        return (rotton == fresh) ? time_max : -1;
     }
 };
