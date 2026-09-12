1b35b9236567d7a5041297b3d74f5628bff0024a
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-12 16:18:56 +0530

    Time: 11 ms (85.02%), Space: 34.9 MB (60.71%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,38 +4,35 @@ public:
         int n = mat.size();
         int m = mat[0].size();
 
-        vector<vector<int>> ans(n, vector<int>(m, -1));
-        queue<pair<int,int>> q;
+        vector<vector<int>>ans(n,vector<int>(m,INT_MAX));
+        queue<pair<int,int>>q;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(mat[i][j] == 0) {
-                    ans[i][j] = 0;
-                    q.push({i, j});
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    ans[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
+        int dx[]={0,0,1,-1};
+        int dy[]={-1,1,0,0};
 
-        int dx[] = {1, -1, 0, 0};
-        int dy[] = {0, 0, 1, -1};
-
-        while(!q.empty()) {
-            auto [i, j] = q.front();
+        while(!q.empty()){
+            auto [i,j]=q.front();
             q.pop();
+            
 
-            for(int d = 0; d < 4; d++) {
-                int ni = i + dx[d];
-                int nj = j + dy[d];
-
-                if(ni >= 0 && nj >= 0 && ni < n && nj < m 
-                   && ans[ni][nj] == -1) {
+                for(int d=0;d<4;d++){
+                    int ni=dx[d]+i;
+                    int nj=dy[d]+j;
                     
-                    ans[ni][nj] = ans[i][j] + 1;
-                    q.push({ni, nj});
+                    if(ni>=0 && nj>=0 &&ni<n && nj<m && ans[ni][nj]==INT_MAX){
+                        ans[ni][nj]=min(ans[ni][nj],1+ans[i][j]);
+                        q.push({ni,nj});
+                    }
                 }
-            }
         }
-
         return ans;
     }
 };
