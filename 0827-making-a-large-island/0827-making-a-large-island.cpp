74db3d8e21dac81a5ecb6f9e64881a2410b2bb6d
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-14 16:15:10 +0530

    Time: 176 ms (75.26%), Space: 91.5 MB (96.02%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,76 +1,115 @@
 class Solution {
+    
 public:
-   
-    int n;
-    int dfs(int i,int j,vector<vector<int>>&grid,int id){
-        grid[i][j]=id;
-        int cnt=1;
-         int dx[]={1,-1,0,0};
-    int dy[]={0,0,-1,1};
-        for(int d=0;d<4;d++){
-            int nc=i+dx[d];
-            int nr=j+dy[d];
 
-            if(nr >= 0 && nc >= 0 && nr < n && nc < n&& grid[nc][nr]==1)
-            {
-                cnt+=dfs(nc,nr,grid,id);
-            }
-        }
-        return cnt;
-            }
+    vector<int> parent, size;
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a == b)
+            return;
+
+        parent[b] = a;
+        size[a] += size[b];
+    }
 
     int largestIsland(vector<vector<int>>& grid) {
-        int dx[]={1,-1,0,0};
-            int dy[]={0,0,-1,1};
-        n=grid.size();
-        vector<int>key;
-        int id=2;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    key.push_back(dfs(i,j,grid,id++));
+
+        int n = grid.size();
+
+        parent.resize(n * n);
+        size.assign(n * n, 1);
+
+        for (int i = 0; i < n * n; i++) {
+            parent[i] = i;
+        }
+
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
+
+        // Step 1: Union all neighbouring 1s
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == 1) {
+
+                    int node = i * n + j;
+
+                    for (int d = 0; d < 4; d++) {
+
+                        int nr = i + dx[d];
+                        int nc = j + dy[d];
+
+                        if (nr >= 0 && nc >= 0 &&
+                            nr < n && nc < n &&
+                            grid[nr][nc] == 1) {
+
+                            int next = nr * n + nc;
+
+                            unite(node, next);
+                        }
+                    }
                 }
             }
         }
 
-        if(key.size()==0)return 1;
-        int ans=1;
+        // Find current largest island
+        int ans = 0;
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
 
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==0){
-                    int cnt=1;
-
-                    for(int d=0;d<4;d++){
-                        int nc=i+dx[d];
-                        int nr=j+dy[d];
-
-                        if(nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nc][nr]!=0 && key[grid[nc][nr]-2]>0)
-                        {
-                         cnt+=key[grid[nc][nr]-2];
-                            key[grid[nc][nr]-2]*=-1;
-                        }
-                    
-                    }
-                    for(int d=0;d<4;d++){
-                        int nc=i+dx[d];
-                        int nr=j+dy[d];
-
-                        if(nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nc][nr]!=0 && key[grid[nc][nr]-2]<0)
-                        {
-                        
-                            key[grid[nc][nr]-2]*=-1;
-                        }
-                    
-                    }
-                    ans=max(ans,cnt);
-
-
+                if (grid[i][j] == 1) {
+                    ans = max(ans, size[find(i * n + j)]);
                 }
             }
         }
-        return ans==1?n*n:ans;
+
+        // Step 2: Try converting every 0 into 1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == 0) {
+
+                    int cnt = 1;
+
+                    // Store roots so same island isn't counted twice
+                    vector<int> roots;
+
+                    for (int d = 0; d < 4; d++) {
+
+                        int nr = i + dx[d];
+                        int nc = j + dy[d];
+
+                        if (nr >= 0 && nc >= 0 &&
+                            nr < n && nc < n &&
+                            grid[nr][nc] == 1) {
+
+                            int root = find(nr * n + nc);
+
+                           
+
+                        if (std::find(roots.begin(), roots.end(), root) == roots.end()) {
+                            roots.push_back(root);
+                            cnt += size[root];
+                        }
+                        }
+                    }
+
+                    ans = max(ans, cnt);
+                }
+            }
+        }
+
+        return ans == 0 ? 1 : ans;
     }
 };
