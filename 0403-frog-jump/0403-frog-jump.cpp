22d5c7f009b8418e1dba50f78422ef7eb265c431
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-15 14:19:36 +0530

    Time: 78 ms (77.26%), Space: 237.6 MB (40.31%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-    bool solve(vector<int>stones,int i,int last,vector<vector<int>>&dp){
-        if(i==stones.size()-1){
-            return true;
-        }
-         if (dp[i][last] != -1)
-            return dp[i][last];
+    bool solve(vector<int>&stones,int i,int last,vector<vector<int>>&dp){
+        if(i==stones.size()-1)return true;
+
+        if(dp[i][last]!=-1)return dp[i][last];
 
         for(int d=-1;d<2;d++){
             int jump=last+d;
-            if (jump <= 0)
-                continue;
+
+            if(jump<=0)continue;
+
             int nextpos=stones[i]+jump;
+
             for(int j=i+1;j<stones.size();j++){
                 if(stones[j]==nextpos){
                     if(solve(stones,j,jump,dp)){
                         return dp[i][last]=true;
                     }
                 }
-                if(stones[j]>nextpos){
-                    break;
-                }
+                if(stones[j]>nextpos)break;
             }
         }
         return dp[i][last]=false;
     }
-   bool canCross(vector<int>& stones) {
+    bool canCross(vector<int>& stones) {
+        int n=stones.size();
 
-        int n = stones.size();
+        if(stones[1]!=1)return false;
 
-        
-        if (stones[1] != 1)
-            return false;
-
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-
-        return solve(stones, 0, 0, dp);
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return solve(stones,0,0,dp);
     }
 };
