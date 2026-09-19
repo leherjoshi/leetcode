c4dc482476e32cebd99264acf9fdc89b820e02b4
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-19 11:33:26 +0530

    Time: 5 ms (74.22%), Space: 71.5 MB (70.15%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
         int ans = 0;
-
-        int left = 0;
         int odd = 0;
+        int left = 0;
         int leftEven = 0;
 
-        for (int right = 0; right < n; right++) {
+        for (int right = 0; right < nums.size(); right++) {
 
             if (nums[right] % 2 == 1) {
                 odd++;
             }
 
-            // We have more than k odds:
-            // remove the first odd and everything before it
+            // More than k odds
             if (odd > k) {
-                while (odd > k) {
-                    if (nums[left] % 2 == 1) {
-                        odd--;
-                    }
+                while (nums[left] % 2 == 0) {
                     left++;
                 }
 
+                left++;          // remove first odd
+                odd--;
                 leftEven = 0;
             }
 
             // Count even numbers before the first odd
-            // inside the current window
             if (odd == k) {
-                int temp = left;
-
-                while (temp < right && nums[temp] % 2 == 0) {
-                    temp++;
+                while (left <= right && nums[left] % 2 == 0) {
+                    leftEven++;
+                    left++;
                 }
 
-                leftEven = temp - left;
                 ans += leftEven + 1;
             }
         }
