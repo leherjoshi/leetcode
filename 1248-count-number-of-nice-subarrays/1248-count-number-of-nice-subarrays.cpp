bb19600a69d32731a9dc4e90aa4bd28810c5a2a9
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-19 11:32:53 +0530

    Time: 6 ms (70.58%), Space: 71.6 MB (47.33%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,22 +1,46 @@
 class Solution {
 public:
-    int atMost(vector<int>&nums,int k){
-        int left=0;
-        int count=0;
-        int sum=0;
-
-        for(int right=0;right<nums.size();right++){
-            if(nums[right]%2!=0)count++;
-            while(count>k){
-                if(nums[left]%2!=0)count--;
-                left++;
-            }
-            sum+=right-left+1;
-
-        }
-        return sum;
-    }
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return atMost(nums,k)-atMost(nums,k-1);
+        int n = nums.size();
+        int ans = 0;
+
+        int left = 0;
+        int odd = 0;
+        int leftEven = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            if (nums[right] % 2 == 1) {
+                odd++;
+            }
+
+            // We have more than k odds:
+            // remove the first odd and everything before it
+            if (odd > k) {
+                while (odd > k) {
+                    if (nums[left] % 2 == 1) {
+                        odd--;
+                    }
+                    left++;
+                }
+
+                leftEven = 0;
+            }
+
+            // Count even numbers before the first odd
+            // inside the current window
+            if (odd == k) {
+                int temp = left;
+
+                while (temp < right && nums[temp] % 2 == 0) {
+                    temp++;
+                }
+
+                leftEven = temp - left;
+                ans += leftEven + 1;
+            }
+        }
+
+        return ans;
     }
 };
