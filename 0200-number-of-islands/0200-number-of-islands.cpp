4a7c68d28c7ce95ff32ce6dc02a06e32bf467419
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-22 20:36:11 +0530

    Time: 32 ms (23.32%), Space: 17.3 MB (52.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<char>>&grid,vector<vector<int>>&vis){
-            if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || vis[i][j]!=-1||grid[i][j]=='0')
-                return;
-            int dx[]={1,-1,0,0};
-            int dy[]={0,0,-1,1};
-            vis[i][j]=1;
-            for(int idx=0;idx<4;idx++){
-                int nx=i+dx[idx];
-                int ny=j+dy[idx];
-                 dfs(nx,ny,grid,vis);
-                
+    void dfs(int i, int j, vector<vector<char>>& grid,
+             vector<vector<int>>& vis) {
+
+        vis[i][j] = 1;
+
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+
+        for(int d = 0; d < 4; d++) {
+            int ni = i + dx[d];
+            int nj = j + dy[d];
+
+            if(ni >= 0 && ni < grid.size() &&
+               nj >= 0 && nj < grid[0].size() &&
+               grid[ni][nj] == '1' &&
+               !vis[ni][nj]) {
+
+                dfs(ni, nj, grid, vis);
             }
+        }
     }
+
     int numIslands(vector<vector<char>>& grid) {
-        int cnt=0;
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,-1));
-        for(int i =0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                 if(grid[i][j]=='1'&&vis[i][j]==-1){
-                    dfs(i,j,grid,vis);
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int cnt = 0;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(grid[i][j] == '1' && !vis[i][j]) {
                     cnt++;
+                    dfs(i, j, grid, vis);
                 }
             }
         }
+
         return cnt;
     }
 };
