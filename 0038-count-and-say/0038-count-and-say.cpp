baf86f965e96027ebed0cc8e76c770c1ce86fdef
LAHAR JOSHI <145357192+leherjoshi@users.noreply.github.com>
2026-09-26 22:43:24 +0530

    Time: 3 ms (81.24%), Space: 9.7 MB (90.72%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     string countAndSay(int n) {
-       
-        int num=1;
-        string res =string(1,'1');
-        for(int i=1;i<n;i++){
-             int cnt=1;
-            string temp="";
-            for(int j=1;j<res.size();j++){
-                if(res[j]==res[j-1]){
+
+        string res = string(1, '1');
+
+        for (int i = 1; i < n; i++) {
+
+            int cnt = 1;
+
+            string temp = "";
+
+            for (int j = 1; j < res.size(); j++) {
+
+                if (res[j] == res[j - 1]) {
                     cnt++;
-                }else{
-                    temp+=to_string(cnt)+res[j-1];
-                    cnt=1;
+                } else {
+                    temp += to_string(cnt) + res[j - 1];
+                    cnt = 1;
                 }
             }
-            temp+=to_string(cnt)+res.back();
-            res=temp;
+
+            temp += to_string(cnt) + res[res.size()-1];
+            res = temp;
         }
         return res;
     }
